f7240fb1176d297bbf70d922fe1af23aeb5d99a2
iremsarica <iremsarica322@gmail.com>
2025-01-05 14:27:10 +0300

    İlk commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,4 +1,5 @@
 package org.example;
+import java.lang.Math;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,22 +7,33 @@ public class Main {
     }
 
     public static boolean shouldWakeUp(boolean isBarking, int clock) {
-        
+      if (clock<0  || clock>23)
+          return  false;
+      return  isBarking && (clock > 20 || clock<8);
+
     }
 
     public static boolean hasTeen(int firstAge, int secondAge, int thirdAge) {
-       
+       return (firstAge>= 13 && firstAge <= 19)||
+               (secondAge>=13 && secondAge<=19)||
+               (thirdAge>=13 && thirdAge<=19);
     }
 
     public static boolean isCatPlaying(boolean isSummer, int temp) {
-     
+     int maxTemp = isSummer ? 45:35 ;
+     return  (temp>=25 && temp<= maxTemp);
     }
 
     public static double area(double width, double height) {
-       
+       if (width<0 || height<0)
+           return -1;
+       return width*height;
     }
 
     public static double area(double radius) {
-       
+       if (radius<0)
+           return  -1;
+
+       return radius*radius*Math.PI;
     }
 }
